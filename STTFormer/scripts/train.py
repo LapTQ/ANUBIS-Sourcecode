730de47e8ce9ec7872aa5893a886aca76fbfee51
LapTQ <lap.tq4@gmail.com>
2026-07-02 16:13:47 +0700

    .

--- STTFormer/scripts/train.py
+++ STTFormer/scripts/train.py
@@ -3,9 +3,13 @@ ls_path_cfg = [
     "v220--satudora_veo3_awlrecord--split-14-class--nodistinct--2s-15frames--v2--b",
     "v221--satudora_veo3_awlrecord--split-14-class--nodistinct--2s-15frames--v2--jm",
     "v222--satudora_veo3_awlrecord--split-14-class--nodistinct--2s-15frames--v2--bm",
+    # "v239--satudora_107cia--2s-15frames--cluster-skeleton-8--j",
+    # "v240--satudora_107cia--2s-15frames--cluster-skeleton-8--b",
+    # "v241--satudora_107cia--2s-15frames--cluster-skeleton-8--jm",
+    # "v242--satudora_107cia--2s-15frames--cluster-skeleton-8--bm",
 ]
 num_models = 10
-ls_devices = [2, 2, 2, 2]
+ls_devices = [0, 0, 0, 0]
 
 import subprocess
 import multiprocessing as mp
