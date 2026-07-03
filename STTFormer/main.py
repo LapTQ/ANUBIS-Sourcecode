cb1078ccee32882b7bf3cda59683abf68dd9bceb
LapTQ <lap.tq4@gmail.com>
2026-07-03 16:13:11 +0700

    .

--- STTFormer/main.py
+++ STTFormer/main.py
@@ -22,10 +22,10 @@ from tqdm import tqdm
 
 
 def init_seed(seed):
-    torch.cuda.manual_seed_all(1)
-    torch.manual_seed(1)
-    np.random.seed(1)
-    random.seed(1)
+    torch.cuda.manual_seed_all(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
     # torch.backends.cudnn.enabled = False
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
